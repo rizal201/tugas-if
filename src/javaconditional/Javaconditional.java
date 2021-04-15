26235f5c83bd76e7d858a0e83c2e1314e771780d
rizal201 <rizalblakekok87@gmail.com>
2021-04-15 16:08:09 +0700

    tugasif

---
+++ src/javaconditional/Javaconditional.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaconditional;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Javaconditional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
